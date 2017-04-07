88289a831c8353d8fe5b6be434673b66f85408e7
bschwartz10 <bschwartz92@gmail.com>
2017-04-06 18:23:18 -0600

    add validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,66 @@
 require 'rails_helper'
 
 describe User do
-  it "user can be created as an admin" do
+  context "admin functionality" do
+    it "user can be created as an admin" do
 
-    user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 1)
+      user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 1)
 
-    expect(user.role).to eq("admin")
-    expect(user.admin?).to be_truthy
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "user can be created as default" do
+
+      user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+
+      expect(user.role).to eq("default")
+      expect(user.admin?).to be_falsey
+    end
   end
 
-  it "user can be created as default" do
+  context "validations" do
+    it "is invalid without a first_name" do
+      user = User.create(last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+      expect(user).to be_invalid
+    end
 
-    user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+    it "is invalid without a last_name" do
+      user = User.create(first_name: "Brett", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+      expect(user).to be_invalid
+    end
 
-    expect(user.role).to eq("default")
-    expect(user.admin?).to be_falsey
+    it "is invalid without an email" do
+      user = User.create(first_name: "Brett", last_name: "schwartz", password: "password", password_confirmation: "password", role: 0)
+      expect(user).to be_invalid
+    end
+
+    it "is invalid without a password" do
+      user = User.create(first_name: "Brett", last_name: "schwartz", email: "bschwartz@example.com", password_confirmation: "password", role: 0)
+      expect(user).to be_invalid
+    end
+
+    it "is invalid without a password_confirmation" do
+      user = User.create(first_name: "Brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", role: 0)
+      expect(user).to be_invalid
+    end
+
+    it "is invalid without a role" do
+      user = User.create(first_name: "Brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password")
+      expect(user).to be_invalid
+    end
+
+    it "is valid without a role" do
+      user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+      expect(user).to be_valid
+    end
+
+    it "has a unique email" do
+      user1 = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+      user2 = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+
+      expect(user2).to be_invalid
+    end
+
   end
 end
