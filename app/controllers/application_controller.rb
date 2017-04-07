3115d478fbce8edb663c88648465cc25861ccdcf
bschwartz10 <bschwartz92@gmail.com>
2017-04-06 18:12:46 -0600

    complete admin_categories_spec tests for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
       User.new(email: "GUEST")
     end
   end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
 end
--- spec/features/admin_categories_spec.rb
+++ spec/features/admin_categories_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Admin visits new categories page" do
+RSpec.describe "User visits new categories page" do
   context "as admin" do
-    it "allows admin to see create a new category" do
+    it "allows admin to create a new category" do
 
       admin = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 1)
 
@@ -12,6 +12,20 @@ RSpec.describe "Admin visits new categories page" do
 
       expect(page).to have_content("Create a new category")
 
+    end
+  end
+
+  context "as user" do
+    it "doesn't allow user to create a new category" do
+
+      user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit new_admin_category_path
+
+      expect(page).to_not have_content("Create a new category")
+      expect(page).to have_content("The page you were looking for doesn't exist")
     end
   end
 end
