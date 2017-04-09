538c56a221b071f64c8716642a854a5dad36a7c3
bschwartz10 <bschwartz92@gmail.com>
2017-04-09 15:25:52 -0600

    unfavorite test

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,15 +1,20 @@
 class GifsController < ApplicationController
   def favorite
     @gif = Gif.find(params[:id])
+    @user = current_user
+
     type = params[:type]
     if type == "favorite"
       current_user.gifs << @gif unless current_user.gifs.include?(@gif)
-      redirect_to :back, notice: "You favorited a #{@gif.category.name}'s gif!"
+      redirect_back(fallback_location: category_path(@gif.category))
+      flash[:notice] = "You favorited a #{@gif.category.name}'s gif!"
     elsif type == "unfavorite"
       current_user.gifs.delete(@gif)
-      redirect_to :back, notice: "Unfavorited #{@gif.category.name}'s gif!"
+      redirect_back(fallback_location: user_path(@user))
+      flash[:notice] = "You unfavorited a #{@gif.category.name}'s gif!"
     else
-       redirect_to :back, notice: 'Nothing happened.'
+      redirect_back(fallback_location: category_path(@gif.category))
+      flash[:notice] = "Nothing happened"
     end
   end
 end
--- spec/features/user_can_unfavorite_a_gif_spec.rb
+++ spec/features/user_can_unfavorite_a_gif_spec.rb
@@ -1,20 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "User visits a categories show page" do
-  context "it allows them to favorite a gif" do
-    it "expects gif to show up on user show page" do
+RSpec.describe "User visits a user show page" do
+  context "it allows them to unfavorite a gif" do
+    it "expects gif to be removed from show page" do
+
       category = Category.create(name: "sports")
       gif = (Gif.create(image_url: "http://giphy.com/embed/bKrynkeJjTKow", category_id: 1))
       user = User.create(first_name: "brett", last_name: "schwartz", email: "bschwartz@example.com", password: "password", password_confirmation: "password", role: 0)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit category_path(category)
-
       click_on "Add to favorites"
 
-      expect(current_path).to eq(category_path(category))
-      expect(page).to have_content("You favorited a #{category.name}'s gif!")
-      expect(user.gifs.count).to eq 1
+      visit user_path(user)
+      click_on "Remove from favorites"
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content("You unfavorited a #{category.name}'s gif!")
+      expect(user.gifs.count).to eq 0
     end
   end
 end
